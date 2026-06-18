f6e44e20cb11baa030098534ee45de274caeaf5b
enteryhj58-web <enteryhj58@gmail.com>
2026-06-18 17:21:46 +0900

    Update app.py

--- app.py
+++ app.py
@@ -6,324 +6,71 @@ st.set_page_config(
     page_title="서양음악사 청음 테스트", page_icon="🎵", layout="centered"
 )
 
-# 🎨 [핵심 포인트] 유튜브 비디오 화면을 눈에 안 보이게 완전히 숨기는 CSS 마술
-st.markdown(
-    """
-    <style>
-    /* 유튜브 플레이어 박스의 높이를 1px로 만들어 화면을 완전히 숨깁니다 */
-    div[data-testid="stVideo"] iframe {
-        height: 1px !important;
-        opacity: 0.01;
-    }
-    /* 오디오처럼 보이도록 제어 바 공간만 최소한으로 남깁니다 */
-    div[data-testid="stVideo"] {
-        height: 60px !important;
-        overflow: hidden;
-        border: 1px solid #ddd;
-        border-radius: 10px;
-        background-color: #f9f9f9;
-        padding: 5px;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True,
-)
-
-
 # =========================================================================
 # 58곡 전곡 데이터 리스트
 # =========================================================================
 @st.cache_data
 def get_music_list():
     return [
-        [
-            "Schubert <Erlkönig> (마왕)",
-            "https://youtu.be/JS91p-vmSf0?si=rrO4kgNvBSHCNcKM",
-            0,
-        ],
-        [
-            "Schumann <Widmung> (헌정)",
-            "https://youtu.be/UXDz0aByZVc?si=lYV30_7MVEG84A5_",
-            0,
-        ],
-        [
-            "Schumann <Träumerei> (꿈)",
-            "https://youtu.be/6z82w0l6kwE?si=8-w8t43qNT58pEx9",
-            0,
-        ],
-        [
-            "Mendelssohn ‘Songs without Words’",
-            "https://youtu.be/Nuq_gGVTg2I?si=ALo_HsGlvupm6yUg",
-            0,
-        ],
-        [
-            "Mendelssohn Violin Concerto in E Minor, 1st Mvt.",
-            "https://youtu.be/SDwKJ6bBXEA?si=sOn_Gf6DduE7_C9S",
-            0,
-        ],
-        [
-            "Paganini Caprice No. 24",
-            "https://youtu.be/loOuh543gaY?si=vL_WNxEE7qS0YecM",
-            0,
-        ],
-        [
-            "Rossini opera <William Tell> overture finale",
-            "https://youtu.be/c7O91GDWGPU?si=wwxoKehQDVZSwb0F",
-            0,
-        ],
-        [
-            "Chopin Nocturne Op. 9 No. 2 (23초부터)",
-            "https://youtu.be/8YQJWHCenJU?si=8t5VMLCQyiSg3Yv8",
-            23,
-        ],
-        [
-            "Chopin Polonaise in A-flat Major, Op. 53 ‘Heroic’",
-            "https://youtu.be/d3IKMiv8AHw?si=Mrix2Kd9r8pzTzN8",
-            0,
-        ],
-        [
-            "Liszt ‘La Campanella’ (Paganini Etude No. 3)",
-            "https://youtu.be/M0U73NRSIkw?si=oR8XxjqGybk2YVXu",
-            0,
-        ],
-        [
-            "Liszt Transcendental Etude No. 4 ‘Mazeppa’",
-            "https://youtu.be/Uk6qyGFYJ9M?si=v_Gw48RQ3V5ZRYQN",
-            0,
-        ],
-        [
-            "Liszt Paganini Etude No. 6",
-            "https://youtu.be/rQndTrMpkz8?si=Uh5-ojY4NbAr1rNI",
-            0,
-        ],
-        [
-            "Berlioz <Symphonie Fantastique> 4th Mvt.",
-            "https://youtu.be/roX70PAu3oA?si=iMRL0guNH2As9sFo",
-            0,
-        ],
-        [
-            "Brahms Symphony No 3, 3rd Mvt.",
-            "https://youtu.be/2tB2SLLnPZg?si=ZCL0RPrhIysPY4ef",
-            0,
-        ],
-        [
-            "Brahms Hungarian Dance No. 5",
-            "https://youtu.be/Nzo3atXtm54?si=l9aT001cU7CxTv6C",
-            0,
-        ],
-        [
-            "Saint-Saëns ‘Swan’ (from <The Carnival of the Animals>)",
-            "https://youtu.be/Wpk7Kx4dt-U?si=SAVCziuzVfvxoC0c",
-            0,
-        ],
-        [
-            "Saint-Saëns ‘Tortoise’ (from <The Carnival of the Animals>)",
-            "https://youtu.be/DfNgNTBDNUs?si=eIUTEpxaJeZnK6aU",
-            0,
-        ],
-        [
-            "Offenbach opera <Orpheus in the Underworld> overture (7분부터)",
-            "https://youtu.be/vEnW5_GTooI?si=I8oZVyWc2UvIS5YV",
-            420,
-        ],
-        [
-            "Saint-Saëns ‘The Aquarium’ (from <The Carnival of the Animals>)",
-            "https://youtu.be/a4lmvXYRfa4?si=Oe564Z2d07GYCXBU",
-            0,
-        ],
-        [
-            "Bizet opera <Carmen> ‘Toreador’ (투우사의 노래)",
-            "https://youtu.be/CoV2YOjFowY?si=1hn9ZSaTF8FSEqpH",
-            0,
-        ],
-        [
-            "Bizet opera <Carmen> ‘Gypsy’",
-            "https://youtu.be/JvjjoZFt26I?si=ctEsFvUoog-GVmJq",
-            0,
-        ],
-        [
-            "Leoncavallo opera <Pagliacci> ‘Vesti la giubba’ (의상을 입어라)",
-            "https://youtu.be/Z0PMq4XGtZ4?si=aFEuc-piN-N1R0V7",
-            0,
-        ],
-        [
-            "Mascagni opera <Cavalleria Rusticana> intermezzo",
-            "https://youtu.be/3N4uXfnH2aA?si=k7giCipOJcc4WQaM",
-            0,
-        ],
-        [
-            "Verdi <Requiem> ‘Dies Irae’",
-            "https://youtu.be/ZcA-KuvHeVE?si=ugrD-QJ-ZwbaKbcc",
-            0,
-        ],
-        [
-            "Verdi opera <Rigoletto> ‘La Donna e mobile’ (여자의 마음)",
-            "https://youtu.be/8A3zetSuYRg?si=DJw9514L78M1Manz",
-            0,
-        ],
-        [
-            "Puccini opera <Turandot> ‘Nessun dorma’ (아무도 잠들지 말라)",
-            "https://youtu.be/M3ecaM3UjAA?si=RpRx03TwklH5_XkI",
-            0,
-        ],
-        [
-            "Puccini opera <Gianni Scchicci> ‘O mio babbino caro’ (사랑하는 나의 아버지)",
-            "https://youtu.be/OrqYQoAIj_0?si=gxmQd9BmRw23uaI3",
-            0,
-        ],
-        [
-            "Dvorak Symphony No. 9 ‘From the New World’ 4th Mvt.",
-            "https://youtu.be/89jOPAGJq-M?si=tiU5MUihec9TWaG5",
-            0,
-        ],
-        [
-            "Grieg Piano Concerto in A Minor, 1st Mvt.",
-            "https://youtu.be/I1Yoyz6_Los?si=qJJ9Jr8GjuOwTPI_",
-            0,
-        ],
-        [
-            "Grieg <Peer Gynt> ‘Morning Mood’",
-            "https://youtu.be/Gf39OSAa6xc?si=AMNQTjvhhn20RjzU",
-            0,
-        ],
-        [
-            "Sibelius Violin Concerto in D Minor, 3rd Mvt. (25분 10초부터)",
-            "https://youtu.be/gpS_u5RvMpM?si=AtCbiggX7CRPMbfW",
-            1510,
-        ],
-        [
-            "Rimsky-Korsakov ‘The Flight of the Bumblebee’",
-            "https://youtu.be/MW8asBxO4oI?si=Na6G0pe-RAugbdeQ",
-            0,
-        ],
-        [
-            "Mussorgsky <Pictures at an Exhibition> ‘Baba Yaga’, ‘Great Gate of Kiev’ (3분 20초부터)",
-            "https://youtu.be/kZkoW1Ta3ew?si=BEmrf4cJaKh57MmA",
-            200,
-        ],
-        [
-            "Tchaikovsky Piano Concerto No. 1, 1st Mvt.",
-            "https://youtu.be/ItSJ_woWnmk?si=fKRRMyIQkYO0oQA4",
-            0,
-        ],
-        [
-            "Tchaikovsky Symphony No. 6 ‘Pathetique’, 1st Mvt. (4분 50초부터)",
-            "https://youtu.be/Hk17wO_mOEU",  # 👈 안정적인 유튜버 링크로 교체 완료
-            290,
-        ],
-        [
-            "Tchaikovsky <The Nutcracker> ‘Waltz of the Flowers’",
-            "https://youtu.be/LKcZL8q1eBw?si=s15yZ_hlN5YlKi5x",
-            0,
-        ],
-        [
-            "Rachmaninoff Symphony No. 2, 3rd Mvt.",
-            "https://youtu.be/21MHVIcg1HY?si=C8j_YwlyvjZMBL1_",
-            0,
-        ],
-        [
-            "Rachmaninoff Piano Concerto No. 3, 3rd Mvt. finale (39분부터)",
-            "https://youtu.be/DPJL488cfRw?feature=shared",
-            2340,
-        ],
-        [
-            "Rachmaninoff <Rhapsody on a Theme of Paganini> 주제 + 제18변주 (16분 20초부터)",
-            "https://youtu.be/3vaJ91DuDww?si=bKci5YQ-b0VdzW_-",
-            980,
-        ],
-        [
-            "Wagner <Die Walküre> ‘The Ride of the Valkyries’",
-            "https://youtu.be/xeRwBiu4wfQ?si=w6HqqS9NQkiaLids",
-            0,
-        ],
-        [
-            "Wagner <Lohengrin> ‘Bridal Chorus’ (3분 20초부터)",
-            "https://youtu.be/J2lX86w-pT4?si=M3Y8t_5-ACM_v63T",
-            200,
-        ],
-        [
-            "Mahler Symphony No. 5, 4th Mvt.",
-            "https://youtu.be/Bj6KLv7kv2Q?si=O2OxPh6iMDGnY_zh",
-            0,
-        ],
-        [
-            "Richard Strauss <Also Sprach Zarathustra>",
-            "https://youtu.be/GfwAPg4rQQE?feature=shared",
-            0,
-        ],
-        [
-            "Johann Strauss <Radetzky March>",
-            "https://youtu.be/xhEGMSOIptw?si=cQ8yYVaOWTV7MxmR",
-            0,
-        ],
-        [
-            "Johann Strauss, Jr. <Frühlingsstimmen Waltz> (봄의 소리 왈츠)",
-            "https://youtu.be/TF0XSkb7TyM?si=zkcbhGx8CPU13siI",
-            0,
-        ],
-        [
-            "Elgar <Salut d’amour> (사랑의 인사)",
-            "https://youtu.be/ysG2AiEFeRw?si=YCKksDGuFiJ-Wb3C",
-            0,
-        ],
-        [
-            "Tarrega <Recuerdos de la Alhambra> (알람브라 궁전의 추억)",
-            "https://youtu.be/EQGBbLBShzk?si=KFBsCP6JCQLzIgw_",
-            0,
-        ],
-        [
-            "Debussy ‘Clair de lune’ (달빛)",
-            "https://youtu.be/U3u4pQ4WKOk?si=WK6Gcs5bG1UoRLW1",
-            0,
-        ],
-        [
-            "Debussy <Prelude to the Afternoon of a Faun> (목신의 오후 전주곡)",
-            "https://youtu.be/9_7loz-HWUM?si=M0KRL-8uRxBWJxX4",
-            0,
-        ],
+        ["Schubert <Erlkönig> (마왕)", "https://youtu.be/JS91p-vmSf0?si=rrO4kgNvBSHCNcKM", 0],
+        ["Schumann <Widmung> (헌정)", "https://youtu.be/UXDz0aByZVc?si=lYV30_7MVEG84A5_", 0],
+        ["Schumann <Träumerei> (꿈)", "https://youtu.be/6z82w0l6kwE?si=8-w8t43qNT58pEx9", 0],
+        ["Mendelssohn ‘Songs without Words’", "https://youtu.be/Nuq_gGVTg2I?si=ALo_HsGlvupm6yUg", 0],
+        ["Mendelssohn Violin Concerto in E Minor, 1st Mvt.", "https://youtu.be/SDwKJ6bBXEA?si=sOn_Gf6DduE7_C9S", 0],
+        ["Paganini Caprice No. 24", "https://youtu.be/loOuh543gaY?si=vL_WNxEE7qS0YecM", 0],
+        ["Rossini opera <William Tell> overture finale", "https://youtu.be/c7O91GDWGPU?si=wwxoKehQDVZSwb0F", 0],
+        ["Chopin Nocturne Op. 9 No. 2 (23초부터)", "https://youtu.be/8YQJWHCenJU?si=8t5VMLCQyiSg3Yv8", 23],
+        ["Chopin Polonaise in A-flat Major, Op. 53 ‘Heroic’", "https://youtu.be/d3IKMiv8AHw?si=Mrix2Kd9r8pzTzN8", 0],
+        ["Liszt ‘La Campanella’ (Paganini Etude No. 3)", "https://youtu.be/M0U73NRSIkw?si=oR8XxjqGybk2YVXu", 0],
+        ["Liszt Transcendental Etude No. 4 ‘Mazeppa’", "https://youtu.be/Uk6qyGFYJ9M?si=v_Gw48RQ3V5ZRYQN", 0],
+        ["Liszt Paganini Etude No. 6", "https://youtu.be/rQndTrMpkz8?si=Uh5-ojY4NbAr1rNI", 0],
+        ["Berlioz <Symphonie Fantastique> 4th Mvt.", "https://youtu.be/roX70PAu3oA?si=iMRL0guNH2As9sFo", 0],
+        ["Brahms Symphony No 3, 3rd Mvt.", "https://youtu.be/2tB2SLLnPZg?si=ZCL0RPrhIysPY4ef", 0],
+        ["Brahms Hungarian Dance No. 5", "https://youtu.be/Nzo3atXtm54?si=l9aT001cU7CxTv6C", 0],
+        ["Saint-Saëns ‘Swan’ (from <The Carnival of the Animals>)", "https://youtu.be/Wpk7Kx4dt-U?si=SAVCziuzVfvxoC0c", 0],
+        ["Saint-Saëns ‘Tortoise’ (from <The Carnival of the Animals>)", "https://youtu.be/DfNgNTBDNUs?si=eIUTEpxaJeZnK6aU", 0],
+        ["Offenbach opera <Orpheus in the Underworld> overture (7분부터)", "https://youtu.be/vEnW5_GTooI?si=I8oZVyWc2UvIS5YV", 420],
+        ["Saint-Saëns ‘The Aquarium’ (from <The Carnival of the Animals>)", "https://youtu.be/a4lmvXYRfa4?si=Oe564Z2d07GYCXBU", 0],
+        ["Bizet opera <Carmen> ‘Toreador’ (투우사의 노래)", "https://youtu.be/CoV2YOjFowY?si=1hn9ZSaTF8FSEqpH", 0],
+        ["Bizet opera <Carmen> ‘Gypsy’", "https://youtu.be/JvjjoZFt26I?si=ctEsFvUoog-GVmJq", 0],
+        ["Leoncavallo opera <Pagliacci> ‘Vesti la giubba’ (의상을 입어라)", "https://youtu.be/Z0PMq4XGtZ4?si=aFEuc-piN-N1R0V7", 0],
+        ["Mascagni opera <Cavalleria Rusticana> intermezzo", "https://youtu.be/3N4uXfnH2aA?si=k7giCipOJcc4WQaM", 0],
+        ["Verdi <Requiem> ‘Dies Irae’", "https://youtu.be/ZcA-KuvHeVE?si=ugrD-QJ-ZwbaKbcc", 0],
+        ["Verdi opera <Rigoletto> ‘La Donna e mobile’ (여자의 마음)", "https://youtu.be/8A3zetSuYRg?si=DJw9514L78M1Manz", 0],
+        ["Puccini opera <Turandot> ‘Nessun dorma’ (아무도 잠들지 말라)", "https://youtu.be/M3ecaM3UjAA?si=RpRx03TwklH5_XkI", 0],
+        ["Puccini opera <Gianni Scchicci> ‘O mio babbino caro’ (사랑하는 나의 아버지)", "https://youtu.be/OrqYQoAIj_0?si=gxmQd9BmRw23uaI3", 0],
+        ["Dvorak Symphony No. 9 ‘From the New World’ 4th Mvt.", "https://youtu.be/89jOPAGJq-M?si=tiU5MUihec9TWaG5", 0],
+        ["Grieg Piano Concerto in A Minor, 1st Mvt.", "https://youtu.be/I1Yoyz6_Los?si=qJJ9Jr8GjuOwTPI_", 0],
+        ["Grieg <Peer Gynt> ‘Morning Mood’", "https://youtu.be/Gf39OSAa6xc?si=AMNQTjvhhn20RjzU", 0],
+        ["Sibelius Violin Concerto in D Minor, 3rd Mvt. (25분 10초부터)", "https://youtu.be/gpS_u5RvMpM?si=AtCbiggX7CRPMbfW", 1510],
+        ["Rimsky-Korsakov ‘The Flight of the Bumblebee’", "https://youtu.be/MW8asBxO4oI?si=Na6G0pe-RAugbdeQ", 0],
+        ["Mussorgsky <Pictures at an Exhibition> ‘Baba Yaga’, ‘Great Gate of Kiev’ (3분 20초부터)", "https://youtu.be/kZkoW1Ta3ew?si=BEmrf4cJaKh57MmA", 200],
+        ["Tchaikovsky Piano Concerto No. 1, 1st Mvt.", "https://youtu.be/ItSJ_woWnmk?si=fKRRMyIQkYO0oQA4", 0],
+        ["Tchaikovsky Symphony No. 6 ‘Pathetique’, 1st Mvt. (4분 50초부터)", "https://youtu.be/Hk17wO_mOEU", 290],
+        ["Tchaikovsky <The Nutcracker> ‘Waltz of the Flowers’", "https://youtu.be/LKcZL8q1eBw?si=s15yZ_hlN5YlKi5x", 0],
+        ["Rachmaninoff Symphony No. 2, 3rd Mvt.", "https://youtu.be/21MHVIcg1HY?si=C8j_YwlyvjZMBL1_", 0],
+        ["Rachmaninoff Piano Concerto No. 3, 3rd Mvt. finale (39분부터)", "https://youtu.be/DPJL488cfRw?feature=shared", 2340],
+        ["Rachmaninoff <Rhapsody on a Theme of Paganini> 주제 + 제18변주 (16분 20초부터)", "https://youtu.be/3vaJ91DuDww?si=bKci5YQ-b0VdzW_-", 980],
+        ["Wagner <Die Walküre> ‘The Ride of the Valkyries’", "https://youtu.be/xeRwBiu4wfQ?si=w6HqqS9NQkiaLids", 0],
+        ["Wagner <Lohengrin> ‘Bridal Chorus’ (3분 20초부터)", "https://youtu.be/J2lX86w-pT4?si=M3Y8t_5-ACM_v63T", 200],
+        ["Mahler Symphony No. 5, 4th Mvt.", "https://youtu.be/Bj6KLv7kv2Q?si=O2OxPh6iMDGnY_zh", 0],
+        ["Richard Strauss <Also Sprach Zarathustra>", "https://youtu.be/GfwAPg4rQQE?feature=shared", 0],
+        ["Johann Strauss <Radetzky March>", "https://youtu.be/xhEGMSOIptw?si=cQ8yYVaOWTV7MxmR", 0],
+        ["Johann Strauss, Jr. <Frühlingsstimmen Waltz> (봄의 소리 왈츠)", "https://youtu.be/TF0XSkb7TyM?si=zkcbhGx8CPU13siI", 0],
+        ["Elgar <Salut d’amour> (사랑의 인사)", "https://youtu.be/ysG2AiEFeRw?si=YCKksDGuFiJ-Wb3C", 0],
+        ["Tarrega <Recuerdos di la Alhambra> (알람브라 궁전의 추억)", "https://youtu.be/EQGBbLBShzk?si=KFBsCP6JCQLzIgw_", 0],
+        ["Debussy ‘Clair de lune’ (달빛)", "https://youtu.be/U3u4pQ4WKOk?si=WK6Gcs5bG1UoRLW1", 0],
+        ["Debussy <Prelude to the Afternoon of a Faun> (목신의 오후 전주곡)", "https://youtu.be/9_7loz-HWUM?si=M0KRL-8uRxBWJxX4", 0],
         ["Ravel <Bolero>", "https://youtu.be/dovA8PuWGLU?si=_FlYYQbSc-qKx4ck", 0],
-        [
-            "Ravel <Jeux d’eau> (물의 유희)",
-            "https://youtu.be/C5kufJw5n3U?si=2nrob-3eaeQ8Aera",
-            0,
-        ],
-        [
-            "Stravinsky <The Rite of Spring>",
-            "https://youtu.be/sPpq44uEyXo?feature=shared",
-            0,
-        ],
-        [
-            "Prokofiev <Romeo and Juliet>",
-            "https://youtu.be/SyDo3h1Tu7c?si=9DBfUroIjznsXVI4",
-            0,
-        ],
-        [
-            "Bartõk <Romanian Folk Dances> (5분 40초부터)",
-            "https://youtu.be/Z50Ooqv1GFg?si=fAPEXl4v9PYyj_LO",
-            340,
-        ],
-        [
-            "Gershwin <Rhapsody in Blue>",
-            "https://youtu.be/cH2PH0auTUU?si=wM_K6FPtYzAgjQuR",
-            0,
-        ],
-        [
-            "Gershwin opera <Porgy and Bess> ‘Summertime’",
-            "https://youtu.be/RyadsHUBpWc?si=xxKA09h413zwZQH1",
-            0,
-        ],
-        [
-            "Shostakovich Waltz No. 2",
-            "https://youtu.be/mmCnQDUSO4I?si=dyNxSa4oa8OMel4p",
-            0,
-        ],
-        [
-            "Piazzolla <Libertango>",
-            "https://youtu.be/t83XjF04r80?si=1_podGTr8-MIOQF-",
-            0,
-        ],
+        ["Ravel <Jeux d’eau> (물의 유희)", "https://youtu.be/C5kufJw5n3U?si=2nrob-3eaeQ8Aera", 0],
+        ["Stravinsky <The Rite of Spring>", "https://youtu.be/sPpq44uEyXo?feature=shared", 0],
+        ["Prokofiev <Romeo and Juliet>", "https://youtu.be/SyDo3h1Tu7c?si=9DBfUroIjznsXVI4", 0],
+        ["Bartõk <Romanian Folk Dances> (5분 40초부터)", "https://youtu.be/Z50Ooqv1GFg?si=fAPEXl4v9PYyj_LO", 340],
+        ["Gershwin <Rhapsody in Blue>", "https://youtu.be/cH2PH0auTUU?si=wM_K6FPtYzAgjQuR", 0],
+        ["Gershwin opera <Porgy and Bess> ‘Summertime’", "https://youtu.be/RyadsHUBpWc?si=xxKA09h413zwZQH1", 0],
+        ["Shostakovich Waltz No. 2", "https://youtu.be/mmCnQDUSO4I?si=dyNxSa4oa8OMel4p", 0],
+        ["Piazzolla <Libertango>", "https://youtu.be/t83XjF04r80?si=1_podGTr8-MIOQF-", 0],
     ]
-
 
 # 상태 초기화 함수
 if "quiz_pool" not in st.session_state:
@@ -346,8 +93,9 @@ if idx < len(pool):
 
     st.subheader(f"📝 문제 {idx + 1} / {len(pool)}")
 
-    # 💡 유튜브 플레이어를 재생하지만 위의 CSS 코드가 화면을 강제로 1px로 짜부라뜨려 소리만 들리게 만듭니다.
-    st.video(url, start_time=int(start_sec))
+    # 💡 [핵심 가림막 박스] 평소에는 굳게 닫혀있어 제목과 화면이 완벽하게 숨겨집니다.
+    with st.expander("▶️ 여기를 눌러 음악 재생하기 (재생 후 상자를 다시 접으면 화면이 완벽히 가려집니다!)", expanded=False):
+        st.video(url, start_time=int(start_sec))
 
     if start_sec > 0:
         st.info(
